76e8a9e152c3a40de4015ec62ecc52acb88bb3f6
nabergh <nab8kd@virginia.edu>
2017-03-14 19:34:25 -0400

    fixed downloading of imgur webpages and albums

--- image_scraper.py
+++ image_scraper.py
@@ -1,8 +1,11 @@
 import json
 import pickle
 import os
-import urllib.request
+import requests
+import shutil
 from multiprocessing import Process, Queue
+post_dir = '../reddit_pics/'
+img_dir = '../reddit_titles_pics/'
 
 def grab_img(post_queue):
 	while True:
@@ -10,21 +13,28 @@ def grab_img(post_queue):
 		if post == None:
 			break
 		try:
-			urllib.request.urlretrieve(post['url'], "/media/tom/shared/reddit_titles_pics/"+post['id'])
+			if '/a/' not in post['url']:
+				if 'imgur' in post['url'] and 'com' in post['url'].split('.')[-1]:
+					post['url'] += '.jpg'
+				resp = requests.get(post['url'])
+				if resp.status_code == 200:
+					with open(img_dir + post['id'] + '.' + resp.headers['content-type'].split('/')[-1], 'wb') as img_file:
+						img_file.write(resp.content)
+				else:
+					print('Status of ' + str(resp.status_code) + ' on post ' + post['id'])
 		except Exception as e:
 			print(e)
 			continue
 
 			
-
 if __name__ == '__main__':
 	grabbed_ids = pickle.load(open("id_set.p","rb"))
 # Multiprocessing to pull the images faster
 	post_queue = Queue()	
-	post_files = os.listdir("reddit_pics/")
+	post_files = os.listdir(post_dir)
 # our JSON from the webscraper
 	for post_json in post_files:
-		posts = json.load(open("reddit_pics/"+post_json,"r"))
+		posts = json.load(open(post_dir + post_json,"r"))
 		for post in posts:
 			if post['id'] in grabbed_ids:
 				continue
