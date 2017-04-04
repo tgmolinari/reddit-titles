7e43a1621d3c9fc33df89f2553faa4407a04e2e2
tgmolinari <tom.molinari12@gmail.com>
2017-04-03 23:47:05 -0400

    modified training loop to generate packed_seqs for the titles

--- train_evaluator.py
+++ train_evaluator.py
@@ -1,5 +1,7 @@
 # Training loop for the evaluator network
-
+import json
+import numpy as np
+import torch
 import torch.autograd as autograd
 import torch.utils.data as data
 import torchvision.transforms as transforms
@@ -12,19 +14,19 @@ from dataset import PostFolder
 def train(model, args : dict):
 	'''
 	args -  img_dir : directory the images live in
-		    posts_json : list of json dicts for reddit posts 
+		    posts_json : filename for the posts json 
             dtype : torch Tensor type
             epochs : number of times to iterate over the training data
 
     model - evaluator network model
     '''
-
+    posts_json = json.load(open(args.posts_json))
     # normalizing function to play nicely with the pretrained feature extractor
 	normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 	# specify the dataloader and dataset generator
 	train_loader = data.DataLoader(
-    	PostFolder(args.posts_json,args.img_dir, transform=transforms.Compose([
+    	PostFolder(posts_json, args.img_dir, transform=transforms.Compose([
        		transforms.ToTensor(),
        		normalize,
     	])),
@@ -34,10 +36,17 @@ def train(model, args : dict):
 	for epoch in args.epochs:
 		total_loss = 0
 		for i, (image, title, score) in enumerate(train_loader):
-			image_var = autograd.Variable(image).type(args.dtype)
-			title_var = autograd.Variable(title).type(args.dtype)
-			score_var = autograd.Variable(score).type(args.dtype)
-			loss_val = model.forward(image_var,title_var,score_var)
+			# because torch doesn't have an argsort, we use numpy's
+			title_lens = np.array(title_lens.tolist())
+			sorted_idx = np.array(np.argsort(title_lens)[::-1])
+			sorted_title_lens = title_lens[sorted_idx]
+
+			image_var = autograd.Variable(image[torch.from_numpy(sorted_idx)]).type(args.dtype)
+			title_var = autograd.Variable(title[torch.from_numpy(sorted_idx)]).type(args.dtype)
+			score_var = autograd.Variable(score[torch.from_numpy(sorted_idx)]).type(args.dtype)
+			packed_seq = pack_padded_sequence(title_var, sorted_title_lens, batch_first = True)
+			
+			loss_val = model.forward(image_var, packed_seq, score_var)
 			total_loss += loss_val
 			model.backward(loss_val)
 			
