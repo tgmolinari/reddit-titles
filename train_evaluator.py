b9a49e5c4c8b4b1e25175f04d426f445bdac89cb
tgmolinari <tom.molinari12@gmail.com>
2017-04-04 22:04:46 -0400

    fixed some type errors, added adaptive learning rate

--- train_evaluator.py
+++ train_evaluator.py
@@ -48,49 +48,53 @@ def train(model, args):
            ])),
        batch_size=args.batch_size, shuffle=True,
        num_workers=8, pin_memory=True)
-
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    learning_rate = 0.001
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     l1_loss = nn.L1Loss()
 
-	batch_ctr = 0
-	for epoch in range(args.epochs):
-		for i, (image, title, title_lens, score) in enumerate(train_loader):
-			
-			# swapping random images and post titles
-			num_mism = int(title.size(0) * MISM_PROP)
-			mism_ind = np.random.choice(title.size(0), num_mism, replace = False)
-			mism_map = mism_ind + randint(0, title.size(0) - 1) % title.size(0)
-			mism_imgs = image[torch.from_numpy(mism_ind)]
-			mism_titles = title[torch.from_numpy(mism_map)]
-			mism_lens = title_lens[torch.from_numpy(mism_map)]
+    batch_ctr = 0
+    epoch_loss = 0
+    for epoch in range(args.epochs):
+        if epoch > 0:
+            last_epoch_loss = epoch_loss
+        for i, (image, title, title_lens, score) in enumerate(train_loader):
 
-			# mangling titles
-			num_mang = int(title.size(0) * MANG_PROP)
-			mang_ind = np.random.choice(title.size(0), num_mang, replace = False) 
-			title_copy = torch.FloatTensor(title)
-			chars_tensor = torch.LongTensor(torch.eye(NUM_CHARS))
-			for ind in mang_ind:
-				num_chars = randint(0, title_lens[ind])
-				mang_chars = np.random.choice(title_lens[ind], num_chars, replace = False)
-				if randint(0, 1) > 0:
-					# uniformly random character substitution
-					title_copy[ind][torch.from_numpy(mang_chars)] = chars_tensor[torch.from_numpy(np.random.randint(NUM_CHARS, num_chars))]
-				else:
-					# randomly change characters to other characters within title
-					title_copy[ind][torch.from_numpy(mang_chars)] = title_copy[ind][torch.from_numpy(np.random.randint(title_lens[ind], num_chars))]
-			mang_imgs = image[torch.from_numpy(mang_ind)]
-			mang_titles = title_copy[torch.from_numpy(mang_ind)]
-			mang_lens = title_lens[torch.from_numpy(mang_ind)]
+            # swapping random images and post titles
+            num_mism = int(title.size(0) * MISM_PROP)
+            mism_ind = np.random.choice(title.size(0), num_mism, replace = False)
+            mism_map = (mism_ind + randint(0, title.size(0) - 1)) % title.size(0)
+            mism_imgs = image[torch.from_numpy(mism_ind)]
+            mism_titles = title[torch.from_numpy(mism_map)]
+            mism_lens = title_lens[torch.from_numpy(mism_map)]
 
-			image = torch.cat((image, mism_imgs, mang_imgs), 0)
-			title = torch.cat((title, mism_titles, mang_titles), 0)
-			title_lens = torch.cat((title_lens, mism_lens, mang_lens), 0)
-			score = torch.cat((score, torch.ones(num_mism + num_mang) * -1), 0)
+            # mangling titles
+            num_mang = int(title.size(0) * MANG_PROP)
+            mang_ind = np.random.choice(title.size(0), num_mang, replace = False) 
+            title_copy = torch.FloatTensor(title)
+            chars_tensor = torch.eye(NUM_CHARS)
+            for ind in mang_ind:
+                num_chars_title = randint(1, title_lens[ind])
+                mang_chars = np.random.choice(title_lens[ind], num_chars_title, replace = False)
+                if randint(0, 1) > 0:
+                    # uniformly random character substitution
+                    title_copy[ind][torch.from_numpy(mang_chars)] = chars_tensor[torch.from_numpy(np.random.choice(NUM_CHARS, num_chars_title))]
+                else:
+                    # randomly change characters to other characters within title
+                    title_copy[ind][torch.from_numpy(mang_chars)] = title_copy[ind][torch.from_numpy(np.random.choice(title_lens[ind], num_chars_title))]
+
+            mang_imgs = image[torch.from_numpy(mang_ind)]
+            mang_titles = title_copy[torch.from_numpy(mang_ind)]
+            mang_lens = title_lens[torch.from_numpy(mang_ind)]
+
+            image = torch.cat((image, mism_imgs, mang_imgs), 0)
+            title = torch.cat((title, mism_titles, mang_titles), 0)
+            title_lens = torch.cat((title_lens, mism_lens, mang_lens), 0)
+            score = torch.cat((score.type(torch.FloatTensor), torch.ones(num_mism + num_mang) * -1), 0)
 
 
-			# because torch doesn't have an argsort, we use numpy's
-			sorted_idx = np.array(np.argsort(title_lens.numpy())[::-1])
-			sorted_title_lens = title_lens[sorted_idx]
+            # because torch doesn't have an argsort, we use numpy's
+            sorted_idx = np.array(np.argsort(title_lens.numpy())[::-1])
+            sorted_title_lens = title_lens.numpy()[sorted_idx]
 
             image_var = autograd.Variable(image[torch.from_numpy(sorted_idx)]).type(args.dtype)
             title_var = autograd.Variable(title[torch.from_numpy(sorted_idx)]).type(args.dtype)
@@ -103,6 +107,8 @@ def train(model, args):
 
             batch_loss = l1_loss(pred_score, score_var)
             log_value('L1 log score loss', batch_loss.data[0], batch_ctr)
+            log_value('Learning rate', optimizer.param_groups[0]['lr'], batch_ctr)
+            epoch_loss += batch_loss.data[0]
             batch_ctr += 1
 
             batch_loss.backward()
@@ -110,6 +116,12 @@ def train(model, args):
 
             if batch_ctr % 1000 == 0:
                 pickle.dump(model.state_dict(), open(args.save_name + '.p', 'wb'))
+        
+        if epoch > 3: #arbitrarily consider hacking the learning rate 
+            if last_epoch_loss/epoch_loss < .0005:
+                learning_rate = learning_rate/2
+                optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
 
 
 parser = argparse.ArgumentParser(description='Evaluator Train')
@@ -129,7 +141,7 @@ parser.add_argument('--epochs', type=int, default=9999999999999,
 if __name__ == '__main__':
     args = parser.parse_args()
     args.dtype = torch.cuda.FloatTensor if torch.cuda.is_available() and args.gpu else torch.FloatTensor
-    
+
     model = ScorePredictor(args.dtype, NUM_CHARS)
 
-	train(model, args)
+    train(model, args)
