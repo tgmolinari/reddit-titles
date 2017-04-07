ebe49902f60ceca59e2835702bd3e2d3e00d5203
tgmolinari <tom.molinari12@gmail.com>
2017-04-06 21:04:34 -0400

    one liner fix to escape potentially null title lens for mangled examples

--- train_evaluator.py
+++ train_evaluator.py
@@ -73,7 +73,7 @@ def train(model, args):
             title_copy = title.clone()
             chars_tensor = torch.eye(NUM_CHARS)
             for ind in mang_ind:
-                num_chars_title = randint(1, title_lens[ind])
+                num_chars_title = randint(1+int(title_lens[ind]*.1), title_lens[ind])
                 mang_chars = np.random.choice(title_lens[ind], num_chars_title, replace = False)
                 if randint(0, 1) > 0:
                     # uniformly random character substitution
